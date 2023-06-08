60fb3dec6041ffd04f5c2277ca1099185f0139cc
Hannah Ahn <ahnlook90@gmail.com>
2023-06-08 23:57:22 +0900

    [#18] FIX: 충돌 해결

--- front-end/src/components/common/ImgBox/ImgBox.tsx
+++ front-end/src/components/common/ImgBox/ImgBox.tsx
@@ -1,5 +1,7 @@
 import { styled } from 'styled-components';
 
+import theme from '../../../styles/theme';
+
 export interface ImgBoxProps {
   src: string;
   width: number;
@@ -10,17 +12,17 @@ const ImgBox = ({ src, width, height }: ImgBoxProps) => {
   return <ImgBoxContainer src={src} alt={src} width={width} height={height} />;
 };
 
-const ImgBoxContainer = styled.img<ImgBoxProps>`
-  width: ${({ width }) => width}px;
-  height: ${({ height }) => height}px;
-  border: 1px solid #ccc;
-  border-radius: 10px;
-  background-color: #f2f2f2;
-`;
-
 ImgBox.defaultProps = {
   width: 120,
   height: 120,
 };
+
+const ImgBoxContainer = styled.img<ImgBoxProps>`
+  width: ${({ width }) => width}px;
+  height: ${({ height }) => height}px;
+  border: 5px solid ${theme.neutral.border};
+  border-radius: 10px;
+  background-color: #f2f2f2;
+`;
 
 export default ImgBox;
