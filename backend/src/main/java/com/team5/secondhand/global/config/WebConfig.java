82468eefe7d29237e0cbb7c39f99d9e05f7a667a
saehim <saeilee.vxv@gmail.com>
2023-06-08 15:29:18 +0900

    [#1] FIX: cours 시그니처 오타 수정

--- backend/src/main/java/com/team5/secondhand/global/config/WebConfig.java
+++ backend/src/main/java/com/team5/secondhand/global/config/WebConfig.java
@@ -1,7 +1,5 @@
 package com.team5.secondhand.global.config;
 
-package team01.issuetracker.config;
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
