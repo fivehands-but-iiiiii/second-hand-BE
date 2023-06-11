f186216b0553eb8f3fcb775c5d33ea99a6a76345
Hannah Ahn <ahnlook90@gmail.com>
2023-06-11 18:32:37 +0900

    [#6] eslint, prettier 설정
    - eslint에 import 룰 등을 추가하였습니다.
    - prettier 상세 설정을 하였습니다.

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,8 +1,12 @@
-import Login, { UserInfo } from '@pages/Login';
 import { useState, createContext } from 'react';
+
 import { ThemeProvider } from 'styled-components';
+
+import Login, { UserInfo } from '@pages/Login';
+
 import GlobalStyle from './styles/globalStyles';
 import theme from './styles/theme';
+
 export const UserInfoContext = createContext({});
 
 function App() {
