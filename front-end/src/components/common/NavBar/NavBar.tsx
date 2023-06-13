42452e6597d44f848a7bd393c2f23a0914a2ba60
NANII <breadnanii@gmail.com>
2023-06-13 21:05:37 +0900

    [#45] FEAT: NavBar 컴포넌트 생성
    - title 영역과 children 영역을 분리하였습니다.
    - 페이지별로 컴포넌트를 props 를 전달받아 렌더링합니다.

---
+++ front-end/src/components/common/NavBar/NavBar.tsx
@@ -0,0 +1,51 @@
+import { ReactNode } from 'react';
+
+import { styled } from 'styled-components';
+
+interface NavBarProps {
+  left?: ReactNode;
+  center?: ReactNode;
+  right?: ReactNode;
+  children?: ReactNode;
+}
+
+const NavBar = ({ left, center, right, children }: NavBarProps) => {
+  return (
+    <MyNavBar>
+      <MyNavBarTitle>
+        {left}
+        <MyNavBarCenter>{center}</MyNavBarCenter>
+        {right}
+      </MyNavBarTitle>
+      {children && <MyNavBarChildren>{children}</MyNavBarChildren>}
+    </MyNavBar>
+  );
+};
+
+const MyNavBar = styled.div`
+  border-bottom: 1px solid ${({ theme }) => theme.colors.neutral.border};
+`;
+
+const MyNavBarTitle = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+  min-height: 66px;
+  padding: 10px 10px;
+  ${({ theme }) => theme.fonts.body};
+  color: ${({ theme }) => theme.colors.neutral.textWeak};
+  /* border-radius: 10px 10px 0px 0px; */ // TODO: 팝업 애니메이션 적용시 필요함
+`;
+
+const MyNavBarCenter = styled.div`
+  font-weight: 600;
+  color: ${({ theme }) => theme.colors.neutral.textStrong};
+`;
+
+const MyNavBarChildren = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 0 10px 10px;
+`;
+
+export default NavBar;
