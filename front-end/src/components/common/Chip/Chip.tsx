63d646a1f00b3f9f1f20816d132d8e6c21a612eb
NANII <breadnanii@gmail.com>
2023-06-13 14:05:23 +0900

    [#37] #ADD: Chip enum 수정
    - item의 상태에 따라서 예약중이거나 예약완료를 나타내는 Chip 컴포넌트를 생성하였습니다. (라벨 삭제)

--- front-end/src/components/common/Chip/Chip.tsx
+++ front-end/src/components/common/Chip/Chip.tsx
@@ -1,19 +1,23 @@
 import { styled } from 'styled-components';
 
 interface ChipProps {
-  status: 'isReserved' | 'isSelected' | 'default';
-  children: string;
+  status: 'reservation' | 'soldOut';
 }
 
-const Chip = ({ status = 'default', children }: ChipProps) => {
-  const ChipTypes = {
-    isReserved: MyReservedChip,
-    isSelected: MySelectedChip,
-    default: MyDefaultChip,
+const Chip = ({ status = 'reservation' }: ChipProps) => {
+  const chipTypes = {
+    reservation: {
+      text: '예약중',
+      style: MyReservedChip,
+    },
+    soldOut: {
+      text: '판매완료',
+      style: MySoldOutChip,
+    },
   };
-  const MyChip = ChipTypes[status];
+  const MyChip = chipTypes[status].style;
 
-  return <MyChip>{children}</MyChip>;
+  return <MyChip>{chipTypes[status].text}</MyChip>;
 };
 
 const MyChip = styled.div`
@@ -21,29 +25,22 @@ const MyChip = styled.div`
   justify-content: center;
   align-items: center;
   width: max-content;
-`;
-
-const MyDefaultChip = styled(MyChip)`
-  height: 32px;
-  padding: 0 16px;
-  border: 1px solid ${({ theme }) => theme.colors.neutral.border};
-  border-radius: 50px;
-  ${({ theme }) => theme.fonts.caption1};
-`;
-
-const MySelectedChip = styled(MyDefaultChip)`
-  border: none;
-  background: ${({ theme }) => theme.colors.accent.backgroundPrimary};
-`;
-
-const MyReservedChip = styled(MyChip)`
   height: 22px;
   padding: 0 8px;
   line-height: 22px;
-  color: ${({ theme }) => theme.colors.accent.text};
   border-radius: 8px;
-  background: ${({ theme }) => theme.colors.accent.backgroundSecondary};
   ${({ theme }) => theme.fonts.caption1};
+`;
+
+const MySoldOutChip = styled(MyChip)`
+  border: 1px solid ${({ theme }) => theme.colors.neutral.backgroundBold};
+  background: ${({ theme }) => theme.colors.neutral.border};
+  color: ${({ theme }) => theme.colors.neutral.textWeak};
+`;
+
+const MyReservedChip = styled(MyChip)`
+  background: ${({ theme }) => theme.colors.accent.backgroundSecondary};
+  color: ${({ theme }) => theme.colors.accent.text};
 `;
 
 export default Chip;
