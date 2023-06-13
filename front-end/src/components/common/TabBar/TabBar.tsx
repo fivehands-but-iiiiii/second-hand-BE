16805aee266830cfe15264620fdb0b9d839d322d
Hannah Ahn <ahnlook90@gmail.com>
2023-06-13 18:47:02 +0900

    [#57] FEAT: tab bar 구현
    - tab bar UI를 구현하였습니다.
    - tab bar 기능을 구현하였습니다.
    - button, a tag의 global style을 추가하였습니다.

--- front-end/src/components/common/TabBar/TabBar.tsx
+++ front-end/src/components/common/TabBar/TabBar.tsx
@@ -1,5 +1,91 @@
-const TabBar = () => {
-  return <div></div>;
+import { useLocation, useNavigate } from 'react-router-dom';
+
+import Icon from '@assets/Icon';
+import * as iconTypes from '@assets/svgs/index';
+import palette from '@styles/colors';
+
+import { styled } from 'styled-components';
+
+import Button from '../Button/Button';
+
+interface TabBarInfo {
+  id: number;
+  icon: keyof typeof iconTypes;
+  label: string;
+  path: string;
+}
+
+interface TabBarStyleProps {
+  tabColor: string;
+}
+
+interface TabBarProps {
+  id: number;
+}
+
+const TabBar = (id: TabBarProps) => {
+  // TODO: 상수는 별도의 파일로 분리 예정
+  const tabBarInfo: TabBarInfo[] = [
+    { id: 1, icon: 'home', label: '홈화면', path: '/' },
+    {
+      id: 2,
+      icon: 'newspaper',
+      label: '판매내역',
+      path: `/items/${id.id}`,
+    },
+    {
+      id: 3,
+      icon: 'heart',
+      label: '관심목록',
+      path: '/wishlist',
+    },
+    {
+      id: 4,
+      icon: 'message',
+      label: '채팅',
+      path: '/messages',
+    },
+    {
+      id: 5,
+      icon: 'person',
+      label: '내 계정',
+      path: '/members',
+    },
+  ];
+
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+  const tabColor = (isClicked: boolean) => {
+    return isClicked
+      ? `${palette.neutral.textStrong}`
+      : `${palette.neutral.textWeak}`;
+  };
+
+  return (
+    <MyTabs>
+      {tabBarInfo.map(({ id, icon, label, path }) => {
+        const isClicked = pathname === path;
+        return (
+          <Button icon fullWidth key={id} onClick={() => navigate(path)}>
+            <Icon name={icon} size="md" fill={tabColor(isClicked)}></Icon>
+            <MyTabLabel tabColor={tabColor(isClicked)}>{label}</MyTabLabel>
+          </Button>
+        );
+      })}
+    </MyTabs>
+  );
 };
+
+const MyTabs = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const MyTabLabel = styled.span<TabBarStyleProps>`
+  color: ${({ tabColor }) => tabColor};
+  font-size: 10px;
+  font-weight: 510;
+`;
 
 export default TabBar;
--- front-end/src/styles/globalStyles.ts
+++ front-end/src/styles/globalStyles.ts
@@ -18,6 +18,11 @@ const GlobalStyle = createGlobalStyle`
     border: none;
     cursor: pointer;
     background-color: transparent;
+    line-height: normal;
+  }
+
+  a {
+    text-decoration: none;
   }
 `;
 
