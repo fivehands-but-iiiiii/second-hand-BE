ac91523457b6d69b71ea8dda9eb47ace935dc92c
jaea-kim <jaeakim98@gmail.com>
2023-06-14 15:53:49 +0900

    [#51] FEAT: github oAuth로 회원가입 하기 구현
    - github api 와 http 통신하기 위해 webClient 를 사용해서 구현

---
+++ backend/src/main/java/com/team5/secondhand/global/oauth/service/OAuthService.java
@@ -0,0 +1,57 @@
+package com.team5.secondhand.global.oauth.service;
+
+import com.team5.secondhand.global.oauth.config.OAuthProperties;
+import com.team5.secondhand.global.oauth.dto.OAuthToken;
+import com.team5.secondhand.global.oauth.dto.UserProfile;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@RequiredArgsConstructor
+@Service
+public class OAuthService {
+    private final OAuthProperties oAuthProperties;
+
+    public UserProfile join(String code) {
+        //access token 가져오기
+        OAuthToken oAuthToken = getToken(code);
+        //github user 정보 가져오기
+        UserProfile user = getUserProfile(oAuthToken);
+
+        return user;
+    }
+
+    private UserProfile getUserProfile(OAuthToken oAuthToken) {
+
+        return WebClient.create()
+                .get()
+                .uri(oAuthProperties.getUserInfoUri())
+                .header("Authorization", oAuthToken.toString())
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToMono(UserProfile.class)
+                .block();
+    }
+
+    private OAuthToken getToken(String code) {
+        MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
+        formData.add("code", code);
+        formData.add("client_id", oAuthProperties.getClientId());
+        formData.add("client_secret", oAuthProperties.getClientSecret());
+        formData.add("redirect_uri", oAuthProperties.getRedirectUri());
+
+        return WebClient.create()
+                .post()
+                .uri(oAuthProperties.getTokenUri())
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .body(BodyInserters.fromFormData(formData))
+                .retrieve()
+                .bodyToMono(OAuthToken.class)
+                .block();
+    }
+}
