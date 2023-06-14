641816108fdae6f287e3661c8bac3791cc181187
saehim <saeilee.vxv@gmail.com>
2023-06-14 16:06:12 +0900

    [#1] 기본 응답 DTO 구현

---
+++ backend/src/main/java/com/team5/secondhand/global/exception/EmptyBasedRegionException.java
@@ -0,0 +1,7 @@
+package com.team5.secondhand.global.exception;
+
+public class EmptyBasedRegionException extends Exception {
+    public EmptyBasedRegionException(String message) {
+        super(message);
+    }
+}
