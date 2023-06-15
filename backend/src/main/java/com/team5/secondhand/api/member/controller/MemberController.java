705f3969433e02d0ed95f218aac39b5d16d28252
saehim <saeilee.vxv@gmail.com>
2023-06-15 16:15:41 +0900

    [#71] FEAT: 회원 가입 url 매핑

--- backend/src/main/java/com/team5/secondhand/api/member/controller/MemberController.java
+++ backend/src/main/java/com/team5/secondhand/api/member/controller/MemberController.java
@@ -1,16 +1,35 @@
 package com.team5.secondhand.api.member.controller;
 
-import com.team5.secondhand.api.member.domain.Member;
+import com.team5.secondhand.api.member.dto.request.BasedRegionSummary;
 import com.team5.secondhand.api.member.dto.request.MemberJoin;
 import com.team5.secondhand.api.member.dto.request.MemberProfileImageUpdate;
 import com.team5.secondhand.api.member.dto.request.MemberRegionUpdate;
-import com.team5.secondhand.api.member.dto.response.MemberDetails;
+import com.team5.secondhand.api.member.service.MemberService;
+import com.team5.secondhand.api.model.Region;
+import com.team5.secondhand.api.region.exception.NotValidRegionException;
+import com.team5.secondhand.api.region.service.RegionService;
+import com.team5.secondhand.global.aws.dto.response.ProfileImageInfo;
+import com.team5.secondhand.global.aws.exception.ImageHostingException;
+import com.team5.secondhand.global.aws.service.ImageHostService;
+import com.team5.secondhand.global.aws.service.ProfileUploadUsecase;
 import com.team5.secondhand.global.dto.GenericResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
+@RequiredArgsConstructor
 public class MemberController {
+
+    private final MemberService memberService;
+    private final RegionService regionService;
+    private final ProfileUploadUsecase profileUploadUsecase;
 
     @Operation(
         summary = "회원가입",
@@ -18,13 +37,22 @@ public class MemberController {
         description = "사용자는 회원가입을 할 수 있다."
     )
     @PostMapping("/join")
-    public GenericResponse<Long> join (MemberJoin request) {
-        //TODO service 호출
+    public GenericResponse<Long> join (@RequestBody MemberJoin request) throws NotValidRegionException {
+        List<Long> ids = request.getRegions().stream()
+                .map(BasedRegionSummary::getId)
+                .collect(Collectors.toList());
+        Map<Long, Region> regions = regionService.getRegions(ids);
+        Map<Region, Boolean> basedRegions = new HashMap<>();
 
-        //TODO response
-        return GenericResponse.send("Member joined Successfully", null);
+        for (BasedRegionSummary regionSummary : request.getRegions()) {
+            Region region = regions.get(regionSummary.getId());
+            basedRegions.put(region, regionSummary.getIsRepresent());
+        }
+
+        Long joinedId = memberService.join(request, basedRegions);
+
+        return GenericResponse.send("Member joined Successfully", joinedId);
     }
-
     @Operation(
         summary = "로그인",
         tags = "Members",
