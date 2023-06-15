fb408f91f81b77e2b531eb36bf94421ebfb665a8
jaea-kim <jaeakim98@gmail.com>
2023-06-15 21:05:35 +0900

    [#81] FEAT: 회원 아이디로 회원정보 찾기 추가

--- backend/src/main/java/com/team5/secondhand/api/member/repository/MemberRepository.java
+++ backend/src/main/java/com/team5/secondhand/api/member/repository/MemberRepository.java
@@ -7,13 +7,17 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     @Modifying
     @Query("UPDATE Member m SET m.profileImgUrl = :url WHERE m.id = :id")
     int updateMemberProfileImage(@Param("id") Long id,
-                                     @Param("url") String url);
+                                 @Param("url") String url);
 
     boolean existsByMemberId(String memberId);
+
+    Optional<Member> findByMemberId(String memberId);
 }
