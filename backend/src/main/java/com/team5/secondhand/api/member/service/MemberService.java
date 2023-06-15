65aa5ec00a4a5dfb27ad1886bce299e37d2ed3a7
jaea-kim <jaeakim98@gmail.com>
2023-06-15 18:40:56 +0900

    [#81] ADD: login 메소드 추가

--- backend/src/main/java/com/team5/secondhand/api/member/service/MemberService.java
+++ backend/src/main/java/com/team5/secondhand/api/member/service/MemberService.java
@@ -3,6 +3,7 @@ package com.team5.secondhand.api.member.service;
 import com.team5.secondhand.api.member.domain.BasedRegion;
 import com.team5.secondhand.api.member.domain.Member;
 import com.team5.secondhand.api.member.dto.request.MemberJoin;
+import com.team5.secondhand.api.member.dto.request.MemberLogin;
 import com.team5.secondhand.api.member.exception.ExistMemberIdException;
 import com.team5.secondhand.api.member.repositoy.MemberBasedRegionRepository;
 import com.team5.secondhand.api.member.repositoy.MemberRepository;
@@ -40,11 +41,20 @@ public class MemberService implements JoinService {
 
         basedRegions.add(BasedRegion.create(newMember, regions.get(0)));
         basedRegions.addAll(regions.stream().skip(1)
-                            .map(region -> BasedRegion.addSubRegion(newMember, region))
-                            .collect(Collectors.toList()));
+                .map(region -> BasedRegion.addSubRegion(newMember, region))
+                .collect(Collectors.toList()));
 
         basedRegionRepository.saveAll(basedRegions);
         return newMember.getId();
     }
 
+    //로그인
+    public void login(MemberLogin loginDTO) {
+        //todo : 레포에 해당 id가 있는지 확인 -> 있으면 성공, 없으면 권한 없음
+
+    }
+
+    public boolean existsByMemberId(String id) {
+        return memberRepository.existsByMemberId(id);
+    }
 }
