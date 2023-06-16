76ecc988e11e2560b943092c9b2ecd4eba0e449a
Hannah Ahn <ahnlook90@gmail.com>
2023-06-16 15:10:28 +0900

    [#90] FEAT: 마지막 아이템 border 제거

--- front-end/src/components/home/ItemList/ItemList.tsx
+++ front-end/src/components/home/ItemList/ItemList.tsx
@@ -1,17 +1,24 @@
 import Item, { SaleItem } from '@common/Item/Item';
 
+import { styled } from 'styled-components';
+
 interface SaleItems {
   saleItems: SaleItem[];
 }
 
 const ItemList = ({ saleItems }: SaleItems) => {
   return (
-    <>
+    <MyItemList>
       {saleItems.map((item) => (
         <Item item={item}></Item>
       ))}
-    </>
+    </MyItemList>
   );
 };
 
+const MyItemList = styled.div`
+  &:last-child {
+    border: none;
+  }
+`;
 export default ItemList;
