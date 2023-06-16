c41c801c1f7aac80d8719521fcc6e946382fad50
saehim <saeilee.vxv@gmail.com>
2023-06-16 18:24:03 +0900

    [#71] FEAT: 프로필 사진 업로드 기능 추가

--- backend/src/main/java/com/team5/secondhand/global/aws/service/ProfileUploadUsecase.java
+++ backend/src/main/java/com/team5/secondhand/global/aws/service/ProfileUploadUsecase.java
@@ -1,6 +1,5 @@
 package com.team5.secondhand.global.aws.service;
 
-import com.team5.secondhand.global.aws.dto.request.ProfileImageUpload;
 import com.team5.secondhand.global.aws.dto.response.ProfileImageInfo;
 import com.team5.secondhand.global.aws.exception.ImageHostingException;
 import org.springframework.web.multipart.MultipartFile;
