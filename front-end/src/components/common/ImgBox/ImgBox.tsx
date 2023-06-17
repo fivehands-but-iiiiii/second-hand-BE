30104b3e41f20ea1995c3c824c4fb3da9b0f7d7d
Hannah Ahn <ahnlook90@gmail.com>
2023-06-17 16:11:35 +0900

    [#18] ImgBox 사이즈 유지를 위한 스타일 변경

--- front-end/src/components/common/ImgBox/ImgBox.tsx
+++ front-end/src/components/common/ImgBox/ImgBox.tsx
@@ -12,13 +12,18 @@ const ImgBox = ({ src, size = 'lg' }: ImgBoxProps) => {
     lg: 120,
   };
   const boxSize = boxType[size];
-  return <MyImgBox src={src} alt={src} width={boxSize} height={boxSize} />;
+  return (
+    <div>
+      <MyImgBox src={src} alt={src} width={boxSize} height={boxSize} />
+    </div>
+  );
 };
 
 const MyImgBox = styled.img<ImgBoxProps>`
   border-radius: 10px;
   background-color: #f2f2f2;
   border: 1px solid ${({ theme }) => theme.colors.neutral.border};
+  object-fit: cover;
 `;
 
 export default ImgBox;
