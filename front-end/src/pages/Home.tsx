385d52c24b34cecf062c54df35fede2acd119333
Hannah Ahn <ahnlook90@gmail.com>
2023-06-17 18:55:35 +0900

    [#90] FEAT: 아이템 자동 fetch 기능 구현

--- front-end/src/pages/Home.tsx
+++ front-end/src/pages/Home.tsx
@@ -1,11 +1,41 @@
-import ItemList from '@components/home/ItemList/ItemList';
+import { useState } from 'react';
 
-import { items } from '../mock/mockData';
+import { SaleItem } from '@common/Item';
+import ItemList from '@components/home/ItemList';
+import useIntersectionObserver from '@hooks/useIntersectionObserver';
+import axios from 'axios';
 
 const Home = () => {
+  const [saleItems, setSaleItems] = useState<SaleItem[]>([]);
+  const [maxPage, setMaxPage] = useState<number>(1);
+  const [page, setPage] = useState<number>(1);
+
+  const onIntersect: IntersectionObserverCallback = ([{ isIntersecting }]) => {
+    if (isIntersecting) fetchItems();
+  };
+
+  const { setTarget } = useIntersectionObserver({ onIntersect });
+
+  const fetchItems = async () => {
+    try {
+      if (page > maxPage) return;
+
+      const { data } = await axios.get(
+        `http://13.125.243.239/items?page=${page}&region=1`,
+      );
+
+      setSaleItems((prevItems) => [...prevItems, ...data.items]);
+      setMaxPage(data.maxPage);
+      setPage((prevPage) => prevPage + 1);
+    } catch (error) {
+      console.error(`Failed to fetch items: ${error}`);
+    }
+  };
+
   return (
     <>
-      <ItemList saleItems={items} />
+      <ItemList saleItems={saleItems} />
+      <div id="onFetchItems" ref={setTarget}></div>
     </>
   );
 };
