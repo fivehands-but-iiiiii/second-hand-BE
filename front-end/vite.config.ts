1c4164ea7198a849e5e9ac4d3a8b556e68d974ff
Hannah Ahn <ahnlook90@gmail.com>
2023-06-17 18:44:58 +0900

    [#90]CHORE: utils 절대 경로 설정 및 axios 설치'

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
       { find: '@constants', replacement: '/src/constants' },
       { find: '@assets', replacement: '/src/assets' },
       { find: '@styles', replacement: '/src/styles' },
+      { find: '@utils', replacement: '/src/utils' },
       { find: '@hooks', replacement: '/src/hooks' },
       {
         find: '@',
