b9bb58b34cabb5182837d8daacce7978ffc143af
NANII <breadnanii@gmail.com>
2023-06-19 03:23:28 +0900

    [#116] Blank 페이지 구현
    - 판매내역, 관심내역이 없을 경우 보여지는 화면을 생성하였습니다.

---
+++ front-end/src/pages/BlankPage.tsx
@@ -0,0 +1,25 @@
+import Icon from '@assets/Icon';
+
+import styled from 'styled-components';
+
+interface BlankPageProps {
+  title: string;
+}
+
+const BlankPage = ({ title }: BlankPageProps) => {
+  return (
+    <MyBlankPage>
+      <span> {title}이 없습니다 </span>
+      <Icon name={'carrot'} size={'sm'} />
+    </MyBlankPage>
+  );
+};
+
+const MyBlankPage = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: ${({ theme }) => theme.colors.neutral.textWeak};
+`;
+
+export default BlankPage;
