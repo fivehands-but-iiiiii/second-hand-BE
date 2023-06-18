f58cbde054464c84578b879c2c15494aefd33e98
jaea-kim <jaeakim98@gmail.com>
2023-06-19 03:10:18 +0900

    [#81] FIX: 응답 바디 수정에 따른 지역 dto 형식 수정
    - 기존에 mainRegion, subRegion id만 보내주는 방식에서 지역 아이디와 상태를 같이 보내주도록 변경

--- backend/src/main/java/com/team5/secondhand/api/member/dto/response/MemberDetails.java
+++ backend/src/main/java/com/team5/secondhand/api/member/dto/response/MemberDetails.java
@@ -1,13 +1,14 @@
 package com.team5.secondhand.api.member.dto.response;
 
-import com.team5.secondhand.api.member.domain.BasedRegion;
 import com.team5.secondhand.api.member.domain.Member;
+import com.team5.secondhand.api.member.dto.request.BasedRegionSummary;
 import com.team5.secondhand.global.exception.EmptyBasedRegionException;
 import lombok.Builder;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Data
 @Builder
@@ -17,17 +18,18 @@ public class MemberDetails {
     private final long id;
     private final String memberId;
     private final String profileImgUrl;
-    private final Long mainRegion;
-    private final Long subRegion;
+    private final List<BasedRegionSummary> regions;
 
     public static MemberDetails fromMember(Member member) throws EmptyBasedRegionException {
+        List<BasedRegionSummary> regions = member.getRegions().stream()
+                .map(region -> BasedRegionSummary.builder().id(region.getId()).onFocus(region.isRepresented()).build())
+                .collect(Collectors.toList());
 
         return MemberDetails.builder()
                 .id(member.getId())
                 .memberId(member.getMemberId())
                 .profileImgUrl(member.getProfileImgUrl())
-                .mainRegion(member.getRepresentedRegionId())
-                .subRegion(member.getSubRegionId())
+                .regions(regions)
                 .build();
     }
 }
