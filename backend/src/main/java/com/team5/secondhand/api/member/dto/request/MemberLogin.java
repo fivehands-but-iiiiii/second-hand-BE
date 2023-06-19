1f8430e1a8aa16ab0743931c0fc7e056afaca68f
jaea-kim <jaeakim98@gmail.com>
2023-06-19 21:06:40 +0900

    [#81] FEAT: 로그인 DTO 수정

--- backend/src/main/java/com/team5/secondhand/api/member/dto/request/MemberLogin.java
+++ backend/src/main/java/com/team5/secondhand/api/member/dto/request/MemberLogin.java
@@ -1,17 +1,13 @@
 package com.team5.secondhand.api.member.dto.request;
 
-import com.team5.secondhand.api.member.domain.Oauth;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 @Getter
 @RequiredArgsConstructor
 public class MemberLogin {
     @NotNull
-    @Size(min = 6, max = 16)
     private final String memberId;
-    private final Oauth oauth;
 }
