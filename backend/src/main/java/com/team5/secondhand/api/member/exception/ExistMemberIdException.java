c6d0002f9258bb692d7b3c38e1ce8709d3844643
saehim <saeilee.vxv@gmail.com>
2023-06-19 16:31:40 +0900

    [#71] FEAT: `MemberExceptionHandler` 외 custom Exception 구현

--- backend/src/main/java/com/team5/secondhand/api/member/exception/ExistMemberIdException.java
+++ backend/src/main/java/com/team5/secondhand/api/member/exception/ExistMemberIdException.java
@@ -1,6 +1,6 @@
 package com.team5.secondhand.api.member.exception;
 
-public class ExistMemberIdException extends Exception {
+public class ExistMemberIdException extends MemberException {
     public ExistMemberIdException(String message) {
         super(message);
     }
