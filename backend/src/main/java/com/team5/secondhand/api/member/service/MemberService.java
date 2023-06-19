9e66674cb01e5fedf60e080d3e6b053491eb2454
saehim <saeilee.vxv@gmail.com>
2023-06-19 16:48:24 +0900

    [#71] FEAT: member data 유효성 검증을 위한 기능
    - 멤버 ID가 존재하는가
    - 멤버 ID 형식이 소문자+숫자로 이루어져 있는가
    - session에 담긴 멤버 데이터와 요청된 github 회원가입 요청이 일치하는가

--- backend/src/main/java/com/team5/secondhand/api/member/service/MemberService.java
+++ backend/src/main/java/com/team5/secondhand/api/member/service/MemberService.java
@@ -8,10 +8,13 @@ import com.team5.secondhand.api.member.dto.request.MemberLogin;
 import com.team5.secondhand.api.member.dto.response.MemberDetails;
 import com.team5.secondhand.api.member.exception.ExistMemberIdException;
 import com.team5.secondhand.api.member.exception.UnauthorizedException;
+import com.team5.secondhand.api.member.exception.UnauthorizedGithubMemberException;
+import com.team5.secondhand.api.member.exception.NotValidMemberIdException;
 import com.team5.secondhand.api.member.repository.MemberBasedRegionRepository;
 import com.team5.secondhand.api.member.repository.MemberRepository;
 import com.team5.secondhand.api.region.domain.Region;
 import com.team5.secondhand.api.oauth.dto.UserProfile;
+import com.team5.secondhand.api.member.exception.MemberDataCorruptedException;
 import com.team5.secondhand.global.exception.EmptyBasedRegionException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -20,19 +23,23 @@ import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 @Service
 @RequiredArgsConstructor
 public class MemberService implements JoinService {
+
+    private final String ID_PATTERN = "^[a-z0-9]+$";
 
     private final MemberRepository memberRepository;
     private final MemberBasedRegionRepository basedRegionRepository;
 
     @Override
     @Transactional
-    public Long join(MemberJoin joinDto, Map<Region, Boolean> regions) {
+    public Long join(MemberJoin joinDto, Map<Region, Boolean> regions, Oauth joinPlatform) {
 
         Member newMember = joinDto.toMember();
+        newMember.updatePlatform(joinPlatform);
         List<BasedRegion> basedRegions = new ArrayList<>();
 
         regions.keySet().forEach(region -> {
@@ -57,19 +64,38 @@ public class MemberService implements JoinService {
         return MemberDetails.fromMember(member);
     }
 
-    public MemberDetails loginByOAuth(UserProfile loginDTO) throws UnauthorizedException, EmptyBasedRegionException {
+    public MemberDetails loginByOAuth(UserProfile loginDTO) throws UnauthorizedGithubMemberException, EmptyBasedRegionException {
         Member member = memberRepository.findByMemberIdAndOauth(loginDTO.getLogin(), Oauth.GITHUB)
-                .orElseThrow(() -> new UnauthorizedException("가입되지 않은 GITHUB 회원입니다", loginDTO));
+                .orElseThrow(() -> new UnauthorizedGithubMemberException("가입되지 않은 GITHUB 회원입니다", loginDTO));
 
         return MemberDetails.fromMember(member);
     }
 
-    public Boolean isExistMemberId(String memberId, Oauth oauth) throws ExistMemberIdException {
+    public boolean isExistMemberId(String memberId, Oauth oauth) {
         return memberRepository.existsByMemberIdAndOauth(memberId, oauth);
     }
 
     @Transactional
     public int updateProfileImage(Long id, String uploadUrl) {
         return memberRepository.updateMemberProfileImage(id,uploadUrl);
+    }
+
+    public void isValidMemberId(String memberId) throws ExistMemberIdException, NotValidMemberIdException {
+        if (isExistMemberId(memberId, Oauth.NONE)) {
+            throw new ExistMemberIdException("이미 존재하는 회원 아이디입니다.");
+        }
+        if (!isFormatMemberId(memberId)) {
+            throw new NotValidMemberIdException("아이디 형식이 올바르지 않습니다.");
+        }
+    }
+
+    private boolean isFormatMemberId(String memberId) {
+        return Pattern.matches(ID_PATTERN, memberId);
+    }
+
+    public void checkDataCorruption(MemberJoin request, UserProfile tempMember) throws MemberDataCorruptedException {
+        if (!request.toMember().equals(tempMember.toMember())) {
+            throw new MemberDataCorruptedException("유효하지 않은 회원가입 요청입니다.");
+        }
     }
 }
