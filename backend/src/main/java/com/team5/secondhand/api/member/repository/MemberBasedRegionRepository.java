6acd50195ce6e75e652dcee160c7a659d172e54e
jaea-kim <jaeakim98@gmail.com>
2023-06-19 18:28:20 +0900

    [#114] REMOVE: 필요없는 클래스 삭제

--- backend/src/main/java/com/team5/secondhand/api/member/repository/MemberBasedRegionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.team5.secondhand.api.member.repository;
-
-import com.team5.secondhand.api.member.domain.BasedRegion;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberBasedRegionRepository extends JpaRepository<BasedRegion, Long> {
-}
