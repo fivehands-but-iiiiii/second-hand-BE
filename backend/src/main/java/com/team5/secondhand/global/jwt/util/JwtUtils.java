8fcb594130165b29436efbc114d67a2038c1bda5
jaea-kim <jaeakim98@gmail.com>
2023-06-20 16:58:46 +0900

    [#135] REFACTOR: 예외처리 메시지 상세히 작성

--- backend/src/main/java/com/team5/secondhand/global/jwt/util/JwtUtils.java
+++ backend/src/main/java/com/team5/secondhand/global/jwt/util/JwtUtils.java
@@ -1,15 +1,12 @@
 package com.team5.secondhand.global.jwt.util;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.team5.secondhand.api.member.dto.response.MemberDetails;
 import io.jsonwebtoken.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.json.JsonParser;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.io.ObjectOutputStream;
 import java.util.Date;
 
 @Component
@@ -33,18 +30,21 @@ public class JwtUtils {
 
     //TODO: refresh token 생성
 
-    public Claims getClaim(String token) {
+    public Claims getClaim(String token) throws JwtException {
         try {
             return Jwts.parser().setSigningKey(jwtProperties.getSecretKey()).parseClaimsJws(token).getBody();
         } catch (MalformedJwtException e) {
             log.error("Invalid JWT token", e);
+            throw new JwtException("토큰이 유효하지 않습니다");
         } catch (ExpiredJwtException e) {
             log.error("Expired JWT token", e);
+            throw new JwtException("토큰이 만료되었습니다.");
         } catch (UnsupportedJwtException e) {
             log.error("Unsupported JWT token", e);
+            throw new JwtException("지원하지 않는 토큰 타입입니다");
         } catch (IllegalArgumentException e) {
             log.error("JWT claims string is empty.", e);
+            throw new JwtException("빈 토큰입니다");
         }
-        throw new JwtException("잘못된 토큰입니당");
     }
 }
