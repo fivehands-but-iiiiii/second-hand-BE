d59255824a750df9674d0205b991ae0b43740666
jaea-kim <jaeakim98@gmail.com>
2023-06-20 17:00:48 +0900

    [#135] RENAME: 클래스 이름 변경

--- backend/src/main/java/com/team5/secondhand/api/region/exception/RegionServiceException.java
+++
@@ -1,7 +0,0 @@
-package com.team5.secondhand.api.region.exception;
-
-public class RegionServiceException extends Exception{
-    public RegionServiceException(String message) {
-        super(message);
-    }
-}
