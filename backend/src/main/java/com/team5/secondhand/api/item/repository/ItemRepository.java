446cede13ff863bb57c492adb4d244c5ad1e7a8a
jaea-kim <jaeakim98@gmail.com>
2023-06-21 22:25:38 +0900

    [#141] FEAT: findById 추가

--- backend/src/main/java/com/team5/secondhand/api/item/repository/ItemRepository.java
+++ backend/src/main/java/com/team5/secondhand/api/item/repository/ItemRepository.java
@@ -5,10 +5,13 @@ import com.team5.secondhand.api.region.domain.Region;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
+    Optional<Item> findById(Long id);
 
     Page<Item> findAllByRegion(Region region, Pageable pageable);
+
     int countAllByRegion(Region region);
 }
