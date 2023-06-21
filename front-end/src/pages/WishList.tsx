98800adb1e1f884a35e01acf7b26621688d1efc4
NANII <breadnanii@gmail.com>
2023-06-21 14:32:55 +0900

    [#115] FEAT: 관심상품 목록 페이지 구현
    - <WishList> 페이지를 라우터에 설정하였습니다.
    - 관심상품 페이지에 카테고리를 클릭하면 쿼리스트링으로 필터된 관심상품을 GET 요청합니다.

--- front-end/src/pages/WishList.tsx
+++ front-end/src/pages/WishList.tsx
@@ -1,5 +1,81 @@
+import { useEffect, useState } from 'react';
+
+import Button from '@common/Button';
+import { SaleItem } from '@common/Item';
+import NavBar from '@common/NavBar';
+import ItemList from '@components/home/ItemList/ItemList';
+
+import { styled } from 'styled-components';
+
+import api from '../api';
+
 const WishList = () => {
-  return <></>;
+  const [wishItems, setWishItems] = useState<SaleItem[]>([]);
+  const [categories, setCategories] = useState([{ id: 0, title: '전체' }]);
+  const [currentCategoryId, setCurrentCategoryId] = useState(0);
+
+  const getWishList = async () => {
+    try {
+      const { data } = await api.get('/wishlist');
+      setWishItems(data.items);
+      setCategories([...categories, data.categories]);
+    } catch (error) {
+      console.error(`Failed to get wishList: ${error}`);
+    }
+  };
+
+  const handleFilterCategories = (categoryId: number) => {
+    setCurrentCategoryId(categoryId);
+    getFilteredItems();
+  };
+
+  const getFilteredItems = async () => {
+    try {
+      const { data } = await api.get(`/wishlist?category=${currentCategoryId}`);
+      setWishItems(data?.items);
+    } catch (error) {
+      console.error(`Failed to filter categories: ${error}`);
+    }
+  };
+
+  useEffect(() => {
+    getWishList();
+  }, []);
+
+  return (
+    <>
+      <NavBar center={'관심 목록'} />
+      <MyWishList>
+        <MyCategories>
+          {categories.map(({ id, title }) => {
+            const isActive = id === currentCategoryId;
+            return (
+              <Button
+                key={id}
+                active={isActive}
+                category
+                onClick={() => handleFilterCategories(id)}
+              >
+                {title}
+              </Button>
+            );
+          })}
+        </MyCategories>
+        <ItemList saleItems={wishItems} />
+      </MyWishList>
+    </>
+  );
 };
+
+const MyWishList = styled.div`
+  padding: 0 2.7vw;
+  height: calc(100%-83px);
+`;
+
+const MyCategories = styled.div`
+  padding: 2vh 0;
+  display: flex;
+  gap: 4px;
+`;
 
 export default WishList;
