3d2fa6fa12ce9e93a6221d65996b2cb2c4659bc7
jaea-kim <jaeakim98@gmail.com>
2023-06-22 19:12:57 +0900

    [#162] FEAT: 도메인에서 DTO 변환 메소드 추가

--- backend/src/main/java/com/team5/secondhand/api/item/dto/request/ItemImage.java
+++ backend/src/main/java/com/team5/secondhand/api/item/dto/request/ItemImage.java
@@ -1,9 +1,11 @@
 package com.team5.secondhand.api.item.dto.request;
 
 import com.team5.secondhand.api.item.domain.ItemDetailImage;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+@Builder
 @Getter
 @RequiredArgsConstructor
 public class ItemImage {
@@ -14,6 +16,13 @@ public class ItemImage {
         return ItemDetailImage.builder()
                 .order(this.order)
                 .url(this.url)
+                .build();
+    }
+
+    public static ItemImage of(ItemDetailImage i) {
+        return ItemImage.builder()
+                .order(i.getOrder())
+                .url(i.getUrl())
                 .build();
     }
 }
