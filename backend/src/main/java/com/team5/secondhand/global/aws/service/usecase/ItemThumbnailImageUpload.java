7416dc338acb9df40fae5f2a492802322a21713f
saehim <saeilee.vxv@gmail.com>
2023-06-22 18:19:23 +0900

    [#157] REFACTOR: 사용하지 않는 import 삭제

--- backend/src/main/java/com/team5/secondhand/global/aws/service/usecase/ItemThumbnailImageUpload.java
+++ backend/src/main/java/com/team5/secondhand/global/aws/service/usecase/ItemThumbnailImageUpload.java
@@ -2,7 +2,6 @@ package com.team5.secondhand.global.aws.service.usecase;
 
 import com.team5.secondhand.global.aws.dto.response.ImageInfo;
 import com.team5.secondhand.global.aws.exception.ImageHostException;
-import org.springframework.web.multipart.MultipartFile;
 
 public interface ItemThumbnailImageUpload {
     ImageInfo uploadItemThumbnailImage(String imageUrl) throws ImageHostException;
