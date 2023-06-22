e4018f8d360351acccd7e009ce60ab62b03bb407
saehim <saeilee.vxv@gmail.com>
2023-06-22 14:52:19 +0900

    [#157] FEAT: DTO 구현

--- backend/src/main/java/com/team5/secondhand/global/aws/dto/request/ItemImageUpload.java
+++ backend/src/main/java/com/team5/secondhand/global/aws/dto/request/ItemImageUpload.java
@@ -1,13 +1,11 @@
 package com.team5.secondhand.global.aws.dto.request;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
-
-@Data
+@Getter
+@RequiredArgsConstructor
 public class ItemImageUpload {
-
-    private final List<MultipartFile> itemImages;
-
+    private final MultipartFile itemImages;
 }
