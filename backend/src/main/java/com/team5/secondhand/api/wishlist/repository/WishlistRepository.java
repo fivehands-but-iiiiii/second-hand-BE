e13704879903b395d2e2a1100861a60ebe1b70dd
jaea-kim <jaeakim98@gmail.com>
2023-06-27 15:23:08 +0900

    [#179] FEAT: 멤버와 아이템으로 위시리스트 찾는 쿼리 메소드 구현

---
+++ backend/src/main/java/com/team5/secondhand/api/wishlist/repository/WishlistRepository.java
@@ -0,0 +1,16 @@
+package com.team5.secondhand.api.wishlist.repository;
+
+import com.team5.secondhand.api.item.domain.Item;
+import com.team5.secondhand.api.member.domain.Member;
+import com.team5.secondhand.api.wishlist.domain.Wishlist;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface WishlistRepository extends JpaRepository<Wishlist, Long> {
+    Optional<Wishlist> findByMemberAndItem(Member member, Item item);
+
+    boolean existsByMemberAndItem(Member member, Item item);
+}
