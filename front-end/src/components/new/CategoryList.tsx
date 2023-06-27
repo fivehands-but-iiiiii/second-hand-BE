5be1d54050c936ad10c80c8b21c80518475b603b
NANII <breadnanii@gmail.com>
2023-06-27 10:12:15 +0900

    [#159] FEAT: 새상품등록 카테고리 연결
    - 제목 영역을 클릭하면 카테고리리스트가 포탈로 나타나게 구현하였습니다.

---
+++ front-end/src/components/new/CategoryList.tsx
@@ -0,0 +1,67 @@
+import NavBar from '@common/NavBar/NavBar';
+import PortalLayout from '@components/layout/PortalLayout';
+
+import { styled } from 'styled-components';
+
+import { Category } from './NewItemEditor';
+interface CategoryListProps {
+  categories: Category[];
+  selectedId: number;
+  onClickCategory: (category: Category) => void;
+  onPortal: () => void;
+}
+
+const CategoryList = ({
+  categories,
+  selectedId,
+  onClickCategory,
+  onPortal,
+}: CategoryListProps) => {
+  return (
+    <PortalLayout>
+      <>
+        <NavBar
+          type={'portal'}
+          left={<button onClick={onPortal}>닫기</button>}
+          center={'카테고리'}
+        ></NavBar>
+        <div>
+          <MyCategoryList>
+            {categories?.map(({ id, title }) => (
+              <MyCategory
+                active={id === selectedId}
+                key={id}
+                onClick={() => onClickCategory({ id, title })}
+              >
+                {title}
+              </MyCategory>
+            ))}
+          </MyCategoryList>
+        </div>
+      </>
+    </PortalLayout>
+  );
+};
+
+const MyCategoryList = styled.ul`
+  position: absolute;
+  width: 100vw;
+  overflow-y: auto;
+  height: 85vh;
+  ${({ theme }) => theme.fonts.subhead}
+  color: ${({ theme }) => theme.colors.neutral.text};
+  > li {
+    height: 6vh;
+    line-height: 6vh;
+    padding: 0 2.7vw;
+    text-align: start;
+    border-bottom: 1px solid ${({ theme }) => theme.colors.neutral.border};
+  }
+`;
+
+const MyCategory = styled.li<{ active: boolean }>`
+  color: ${({ theme, active }) =>
+    active ? theme.colors.accent.backgroundPrimary : theme.colors.neutral.text};
+`;
+
+export default CategoryList;
