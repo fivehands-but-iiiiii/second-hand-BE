939ecbf1e9c6fa9575e2cf22e453cff2f7add3eb
jaea-kim <jaeakim98@gmail.com>
2023-06-27 20:21:39 +0900

    [#179] FEAT: 카테고리를 기준으로 관심목록 조회하는 쿼리 구현

--- backend/src/main/java/com/team5/secondhand/api/wishlist/repository/WishlistRepository.java
+++ backend/src/main/java/com/team5/secondhand/api/wishlist/repository/WishlistRepository.java
@@ -3,13 +3,21 @@ package com.team5.secondhand.api.wishlist.repository;
 import com.team5.secondhand.api.item.domain.Item;
 import com.team5.secondhand.api.member.domain.Member;
 import com.team5.secondhand.api.wishlist.domain.Wishlist;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface WishlistRepository extends JpaRepository<Wishlist, Long> {
+
+    @Query("SELECT w FROM Wishlist w WHERE (:itemCategory IS NULL OR w.item.category = :itemCategory) ORDER BY w.id")
+    Slice<Wishlist> findAllByItemCategoryOrderById(Pageable page, Long itemCategory);
     Optional<Wishlist> findByMemberAndItem(Member member, Item item);
 
     boolean existsByMemberAndItem(Member member, Item item);
