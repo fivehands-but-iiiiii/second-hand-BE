d6de6d5efa66419e40d405e3a3b481c15470fd10
jaea-kim <jaeakim98@gmail.com>
2023-06-28 21:04:40 +0900

    [#213] FIX: 관심목록 응답에 페이지 번호 추가

--- backend/src/main/java/com/team5/secondhand/api/wishlist/dto/response/WishItemList.java
+++ backend/src/main/java/com/team5/secondhand/api/wishlist/dto/response/WishItemList.java
@@ -10,12 +10,14 @@ import java.util.List;
 @Builder
 @RequiredArgsConstructor
 public class WishItemList {
+    private final int page;
     private final boolean hasPrevious;
     private final boolean hasNext;
     private final List<WishItem> items;
 
-    public static WishItemList of(List<WishItem> items, boolean hasNext, boolean hasPrevious) {
+    public static WishItemList of(List<WishItem> items, int page, boolean hasNext, boolean hasPrevious) {
         return WishItemList.builder()
+                .page(page)
                 .items(items)
                 .hasNext(hasNext)
                 .hasPrevious(hasPrevious)
