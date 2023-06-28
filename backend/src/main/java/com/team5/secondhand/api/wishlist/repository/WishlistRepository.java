3c797d1c8fa082c2a10f1c968f60bade305d95f6
Dewey <115435482+jaea-kim@users.noreply.github.com>
2023-06-28 21:10:52 +0900

    Revert "API 버그 수정"

--- backend/src/main/java/com/team5/secondhand/api/wishlist/repository/WishlistRepository.java
+++ backend/src/main/java/com/team5/secondhand/api/wishlist/repository/WishlistRepository.java
@@ -16,12 +16,12 @@ import java.util.Optional;
 @Repository
 public interface WishlistRepository extends JpaRepository<Wishlist, Long> {
 
-    @Query("SELECT w FROM Wishlist w WHERE (:itemCategory IS NULL OR w.item.category = :itemCategory) AND (w.member.id = :memberId) ORDER BY w.id DESC")
-    Slice<Wishlist> findAllByItemCategoryAndMemberIdOrderById(Pageable page, Long itemCategory, Long memberId);
+    @Query("SELECT w FROM Wishlist w WHERE (:itemCategory IS NULL OR w.item.category = :itemCategory) ORDER BY w.id DESC")
+    Slice<Wishlist> findAllByItemCategoryOrderById(Pageable page, Long itemCategory);
     Optional<Wishlist> findByMemberAndItem(Member member, Item item);
+
+    boolean existsByMemberAndItem(Member member, Item item);
 
     @Query("SELECT DISTINCT w.item.category from Wishlist w where w.member.id = :memberId")
     List<Long> findByDistinctCategoryByMember(@Param("memberId") Long memberId);
-
-    boolean existsByMemberIdAndItemId(Long memberId, Long itemId);
 }
