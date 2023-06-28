99f114a44e31ebd597f16639431c8af502c4b85e
jaea-kim <jaeakim98@gmail.com>
2023-06-28 21:33:28 +0900

    [#213] REFACTOR: ErrorResponseWithBody의 body 타입 Generic 타입으로 변경

--- backend/src/main/java/com/team5/secondhand/api/member/exception/UnauthorizedGithubMemberException.java
+++ backend/src/main/java/com/team5/secondhand/api/member/exception/UnauthorizedGithubMemberException.java
@@ -1,13 +1,14 @@
 package com.team5.secondhand.api.member.exception;
 
+import com.team5.secondhand.api.oauth.dto.UserProfile;
 import lombok.Getter;
 
 @Getter
 public class UnauthorizedGithubMemberException extends MemberException {
 
-    private Object body;
+    private UserProfile body;
 
-    public UnauthorizedGithubMemberException(String message, Object body) {
+    public UnauthorizedGithubMemberException(String message, UserProfile body) {
         super(message);
         this.body = body;
     }
