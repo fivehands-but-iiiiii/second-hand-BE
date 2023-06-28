6e7b8c6c3c51914074bb055c33d7149d2cbb8019
NANII <breadnanii@gmail.com>
2023-06-29 02:32:45 +0900

    [#63] FEAT: SegmentedControl 컴포넌트 구현
    - NavBar 상단의 SegmentedControl 컴포넌트를 생성하였습니다.

--- front-end/src/pages/SalesHistory.tsx
+++ front-end/src/pages/SalesHistory.tsx
@@ -1,16 +1,39 @@
-import { useParams } from 'react-router-dom';
+import { useState } from 'react';
 
 import NavBar from '@common/NavBar/NavBar';
+import SegmentedControl from '@common/SegmentedControl';
+import { ItemStatus } from '@components/ItemStatus';
 
 import BlankPage from './BlankPage';
 
+const SALES_STATUS = [
+  {
+    status: ItemStatus.ON_SALE,
+    label: '판매중',
+  },
+  {
+    status: ItemStatus.SOLD_OUT,
+    label: '판매완료',
+  },
+];
+
 const SalesHistory = () => {
-  const { id } = useParams;
+  const [selectedIndex, setSelectedIndex] = useState(ItemStatus.ON_SALE);
   const title = '판매 내역';
-  // TODO: 로그인 Id 를 기반으로 판매내역 조회
+
+  const handleSelectedIndex = (index: number) => {
+    setSelectedIndex(index);
+  };
+
   return (
     <>
-      <NavBar center={title} />
+      <NavBar center={title}>
+        <SegmentedControl
+          options={SALES_STATUS}
+          value={selectedIndex}
+          onClick={handleSelectedIndex}
+        />
+      </NavBar>
       <BlankPage title={title} />
     </>
   );
