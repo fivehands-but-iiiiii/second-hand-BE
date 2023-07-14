583649237eaec01d3d99349b8033dff7e6d732a8
jaea-kim <jaeakim98@gmail.com>
2023-07-14 17:57:02 +0900

    [#260] FEAT: 아이템과 구매자로 채팅방 찾기 구현

--- backend/src/main/java/com/team5/secondhand/api/chatroom/repository/ChatRoomRepository.java
+++ backend/src/main/java/com/team5/secondhand/api/chatroom/repository/ChatRoomRepository.java
@@ -1,10 +1,12 @@
 package com.team5.secondhand.api.chatroom.repository;
 
 import com.team5.secondhand.api.chatroom.domian.ChatRoom;
+import com.team5.secondhand.api.item.domain.Item;
+import com.team5.secondhand.api.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
-    Optional<ChatRoom> findByItemId(Long itemId);
+    Optional<ChatRoom> findByItemAndBuyer(Item item, Member buyer);
 }
