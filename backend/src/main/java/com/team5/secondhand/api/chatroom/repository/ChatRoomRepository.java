bf99dc612de7206d27d55b8764a9ddc8c8996dbc
jaea-kim <jaeakim98@gmail.com>
2023-07-14 17:35:43 +0900

    [#260] FEAT: 채팅방 레포지토리 생성

---
+++ backend/src/main/java/com/team5/secondhand/api/chatroom/repository/ChatRoomRepository.java
@@ -0,0 +1,10 @@
+package com.team5.secondhand.api.chatroom.repository;
+
+import com.team5.secondhand.api.chatroom.domian.ChatRoom;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+    Optional<ChatRoom> findByItemId(Long itemId);
+}
