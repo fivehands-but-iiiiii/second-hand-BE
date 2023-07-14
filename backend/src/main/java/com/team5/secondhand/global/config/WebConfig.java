f7f9c12d23f4f0d14373bafd1fb7ab244c5a6807
saehim <saeilee.vxv@gmail.com>
2023-07-14 15:17:54 +0900

    [#244] FIX: WebConfig cors 수정

--- backend/src/main/java/com/team5/secondhand/global/config/WebConfig.java
+++ backend/src/main/java/com/team5/secondhand/global/config/WebConfig.java
@@ -28,7 +28,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins(
                         "http://127.0.0.1:5173", "http://localhost:5173",
-                        "http://3.37.51.148:81/",
+                        "http://3.37.51.148:80",
                         FRONTEND_SERVER, DOMAIN
                 )
                 .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS", "HEAD")
