4b99eb1f62d6a206e478730b2266b00fabfc3b48
saehim <saeilee.vxv@gmail.com>
2023-07-19 15:21:32 +0900

    [#273] TEST: origin cors 테스트

--- backend/src/main/java/com/team5/secondhand/global/config/WebConfig.java
+++ backend/src/main/java/com/team5/secondhand/global/config/WebConfig.java
@@ -26,10 +26,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(
-                        "http://127.0.0.1:5173", "http://localhost:5173",
-                        "http://3.37.51.148",
-                        FRONTEND_SERVER, DOMAIN
+                .allowedOrigins("*"
+//                        "http://127.0.0.1:5173", "http://localhost:5173",
+//                        "http://3.37.51.148",
+//                        FRONTEND_SERVER, DOMAIN
                 )
                 .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS", "HEAD")
                 .allowedHeaders("*")
