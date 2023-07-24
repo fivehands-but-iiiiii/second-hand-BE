686b2b5a7d77d2516f82ee53399b8a4437ff830a
jaea-kim <jaeakim98@gmail.com>
2023-07-24 17:57:50 +0900

    [#260] CHORE: 채팅방 리스트 조회 시 페이지 번호 필수로 수정

--- backend/src/main/java/com/team5/secondhand/api/chatroom/dto/request/ChatItem.java
+++ backend/src/main/java/com/team5/secondhand/api/chatroom/dto/request/ChatItem.java
@@ -3,9 +3,12 @@ package com.team5.secondhand.api.chatroom.dto.request;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import javax.validation.constraints.Size;
+
 @Getter
 @RequiredArgsConstructor
 public class ChatItem {
     private final Long itemId;
-    private final Integer page;
+    @Size(min = 0)
+    private final int page;
 }
