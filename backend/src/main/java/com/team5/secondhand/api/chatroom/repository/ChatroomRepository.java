861a741127f95f777c3a144f05900d5dcfbd78cf
jaea-kim <jaeakim98@gmail.com>
2023-07-24 17:43:45 +0900

    [#260] CHORE: 채팅방ID 타입 변경에 따른 수정

--- backend/src/main/java/com/team5/secondhand/api/chatroom/repository/ChatroomRepository.java
+++ backend/src/main/java/com/team5/secondhand/api/chatroom/repository/ChatroomRepository.java
@@ -5,10 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 public interface ChatroomRepository extends JpaRepository<Chatroom, Long> {
     Optional<Chatroom> findByItemId(Long itemId);
-    Optional<Chatroom> findByChatroomId(String chatroomId);
+    Optional<Chatroom> findByChatroomId(UUID chatroomId);
     Optional<Chatroom> findByBuyer_IdAndItem_Id(Long buyerId, Long itemId);
     List<Chatroom> findAllByBuyerIdOrSellerIdOrderByIdDesc(Long buyerId, Long sellerId);
     List<Chatroom> findAllByItemId(Long itemId);
--- backend/src/main/java/com/team5/secondhand/api/chatroom/service/ChatroomService.java
+++ backend/src/main/java/com/team5/secondhand/api/chatroom/service/ChatroomService.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Service
@@ -38,7 +39,7 @@ public class ChatroomService {
     }
 
     public Optional<Chatroom> findByChatroomId(String chatroomId) {
-        return chatRoomRepository.findByChatroomId(chatroomId);
+        return chatRoomRepository.findByChatroomId(UUID.fromString(chatroomId));
     }
 
     public ChatroomList findChatroomListByMember(Member member) {
