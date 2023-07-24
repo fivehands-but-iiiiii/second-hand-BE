c0ae4613a7320f135114d56ce23da323590fdb96
jaea-kim <jaeakim98@gmail.com>
2023-07-24 13:18:21 +0900

    [#260] FEAT: 채팅방 목록 조회 레포지토리 인터페이스 추가

--- backend/src/main/java/com/team5/secondhand/api/chatroom/repository/ChatroomRepository.java
+++ backend/src/main/java/com/team5/secondhand/api/chatroom/repository/ChatroomRepository.java
@@ -1,15 +1,15 @@
 package com.team5.secondhand.api.chatroom.repository;
 
 import com.team5.secondhand.api.chatroom.domian.Chatroom;
-import com.team5.secondhand.api.item.domain.Item;
-import com.team5.secondhand.api.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ChatroomRepository extends JpaRepository<Chatroom, Long> {
     Optional<Chatroom> findByItemId(Long itemId);
     Optional<Chatroom> findByChatroomId(String chatroomId);
     Optional<Chatroom> findByBuyer_IdAndItem_Id(Long buyerId, Long itemId);
-    Optional<Chatroom> findByItemAndBuyer(Item item, Member buyer);
+    List<Chatroom> findAllByBuyerIdOrSellerIdOrderByIdDesc(Long buyerId, Long sellerId);
+    List<Chatroom> findAllByItemId(Long itemId);
 }
