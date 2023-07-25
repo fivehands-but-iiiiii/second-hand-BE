8a57c50128d4e8c9b002a5a2ee7dec0f988ea816
NANII <breadnanii@gmail.com>
2023-07-25 17:01:59 +0900

    [#298] FEAT: Alert UI 컴포넌트 생성
    - 컴파운드컴포넌트를 사용하여 구현하였습니다.
    
    Co-authored-by: ahnlook <ahnlook90@gmail.com>

---
+++ front-end/src/components/common/Alert/constants.ts
@@ -0,0 +1,25 @@
+export const ALERT_TITLE = {
+  DELETE: (subject: string) => `${subject}을 삭제하시겠어요?`,
+  EXIT: '채팅방을 나가시겠어요?',
+  LOGOUT: '로그아웃을 하시겠어요?',
+  LOGIN: '로그인이 필요해요.',
+};
+
+export const ALERT_ACTIONS = {
+  DELETE: [
+    { id: 1, action: '취소' },
+    { id: 2, action: '삭제' },
+  ],
+  EXIT: [
+    { id: 1, action: '취소' },
+    { id: 2, action: '나가기' },
+  ],
+  LOGOUT: [
+    { id: 1, action: '취소' },
+    { id: 2, action: '로그아웃' },
+  ],
+  LOGIN: [
+    { id: 1, action: '홈으로' },
+    { id: 2, action: '로그인' },
+  ],
+};
