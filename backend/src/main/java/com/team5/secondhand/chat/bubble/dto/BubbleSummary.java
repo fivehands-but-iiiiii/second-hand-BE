20b55fac65ebef01612368b7f008dfb325f714fe
saehim <saeilee.vxv@gmail.com>
2023-07-25 15:58:39 +0900

    [#280] REFACTOR: DTO 데이터 추가

--- backend/src/main/java/com/team5/secondhand/chat/bubble/dto/BubbleSummary.java
+++ backend/src/main/java/com/team5/secondhand/chat/bubble/dto/BubbleSummary.java
@@ -3,29 +3,36 @@ package com.team5.secondhand.chat.bubble.dto;
 import com.team5.secondhand.api.member.dto.response.MemberDetails;
 import com.team5.secondhand.chat.bubble.domain.ChatBubble;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.time.Instant;
+
+@Getter
 @RequiredArgsConstructor
 public class BubbleSummary {
     private String id;
     private String senderId;
     private String contents;
     private Boolean isMine;
+    private Instant createdAt;
 
     @Builder
-    private BubbleSummary(String id, String senderId, String contents, Boolean isMine) {
+    private BubbleSummary(String id, String senderId, String contents, Boolean isMine, Instant createdAt) {
         this.id = id;
         this.senderId = senderId;
         this.contents = contents;
         this.isMine = isMine;
+        this.createdAt = createdAt;
     }
 
     public static BubbleSummary from(ChatBubble bubble, MemberDetails loginMember) {
         return BubbleSummary.builder()
                 .id(bubble.getId())
                 .senderId(bubble.getFrom())
-                .contents(bubble.getContents())
+                .contents(bubble.getMessage())
                 .isMine(bubble.isSender(loginMember))
+                .createdAt(Instant.parse(bubble.getCreatedAt()))
                 .build();
     }
 }
