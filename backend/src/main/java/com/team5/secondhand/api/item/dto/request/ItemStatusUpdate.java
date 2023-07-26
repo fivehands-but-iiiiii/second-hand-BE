0f6d488b8aac14920622c023d8bc6b1fb2165822
saehim <saeilee.vxv@gmail.com>
2023-07-26 11:22:55 +0900

    [#280] FIX: field 1개인 DTO 생성자 변경

--- backend/src/main/java/com/team5/secondhand/api/item/dto/request/ItemStatusUpdate.java
+++ backend/src/main/java/com/team5/secondhand/api/item/dto/request/ItemStatusUpdate.java
@@ -3,7 +3,6 @@ package com.team5.secondhand.api.item.dto.request;
 import com.team5.secondhand.api.item.domain.Status;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 
 @Getter
 @NoArgsConstructor
