1e4ac0e22b021e1bd7588c4d6128187facd05fac
jaea-kim <jaeakim98@gmail.com>
2023-07-27 17:11:19 +0900

    [#313] REFACTOR: github oauth 환경이 기본 github oauth 를 상속받도록 변경

--- backend/src/main/java/com/team5/secondhand/api/oauth/config/OAuthLocalProperties.java
+++ backend/src/main/java/com/team5/secondhand/api/oauth/config/OAuthLocalProperties.java
@@ -1,15 +1,9 @@
 package com.team5.secondhand.api.oauth.config;
 
-import lombok.*;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-@Getter @Setter
 @Configuration
 @ConfigurationProperties(prefix = "oauth.env.local")
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class OAuthLocalProperties {
-    private String clientId;
-    private String clientSecret;
-    private String redirectUri;
+public class OAuthLocalProperties extends OAuthProperties{
 }
