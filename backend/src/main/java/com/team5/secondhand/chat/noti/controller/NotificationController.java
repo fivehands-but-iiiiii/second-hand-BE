48252c9e04c7cd70b92151c64a82be2b0cd2eee2
saehim <saeilee.vxv@gmail.com>
2023-07-28 23:46:55 +0900

    [#329] FEAT: 알람을 위한 controller, service, repository 정의

---
+++ backend/src/main/java/com/team5/secondhand/chat/noti/controller/NotificationController.java
@@ -0,0 +1,20 @@
+package com.team5.secondhand.chat.noti.controller;
+
+import com.team5.secondhand.api.member.dto.response.MemberDetails;
+import com.team5.secondhand.chat.noti.service.NotificationService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import javax.servlet.http.HttpServletResponse;
+import java.net.http.HttpResponse;
+
+@Slf4j
+@RestController
+@RequestMapping("/chats")
+@RequiredArgsConstructor
+public class NotificationController {
+    private final NotificationService notificationService;
+
+}
