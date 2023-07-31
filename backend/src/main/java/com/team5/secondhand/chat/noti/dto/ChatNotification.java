98dfeb1ca1264f56a72f3a873e7790b19141afb9
saehim <saeilee.vxv@gmail.com>
2023-07-31 16:24:54 +0900

    [#329] FEAT: 채팅 알람 DTO 임시 구현

---
+++ backend/src/main/java/com/team5/secondhand/chat/noti/dto/ChatNotification.java
@@ -0,0 +1,16 @@
+package com.team5.secondhand.chat.noti.dto;
+
+import com.team5.secondhand.chat.bubble.domain.ChatBubble;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class ChatNotification {
+    private final String roomId;
+    private final String message;
+
+    public static ChatNotification of(ChatBubble message) {
+        return new ChatNotification(message.getRoomId(), message.getMessage());
+    }
+}
