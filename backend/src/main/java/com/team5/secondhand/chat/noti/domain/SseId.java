959ebea7d20744935a0e27bacf67c21f99ad0ad3
saehim <saeilee.vxv@gmail.com>
2023-07-31 16:23:24 +0900

    [#329] FEAT: sseId domain으로 구현

---
+++ backend/src/main/java/com/team5/secondhand/chat/noti/domain/SseId.java
@@ -0,0 +1,37 @@
+package com.team5.secondhand.chat.noti.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.Instant;
+
+@Getter
+public class SseId {
+    private final String id;
+    private final String memberId;
+    private final Instant createdAt;
+
+    @Builder
+    private SseId(String id, String memberId, Instant createdAt) {
+        this.id = id;
+        this.memberId = memberId;
+        this.createdAt = createdAt;
+    }
+
+    public static SseId of(String memberId) {
+        Instant createdAt = Instant.now();
+        return SseId.builder()
+                .id(generateId(memberId, createdAt))
+                .memberId(memberId)
+                .createdAt(createdAt)
+                .build();
+    }
+
+    private static String generateId(String memberId, Instant createdAt) {
+        return String.format("%s_%d",memberId,createdAt.getEpochSecond());
+    }
+
+    public boolean startWith(String memberId) {
+        return this.memberId.equals(memberId);
+    }
+}
