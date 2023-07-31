57258d83181b4e2369c87e58a4240e9d642aa12f
saehim <saeilee.vxv@gmail.com>
2023-07-31 16:25:55 +0900

    [#329] TEST: SSE 메시지 알람 테스트를 위해 임시로 코드 추가

--- backend/src/main/java/com/team5/secondhand/chat/bubble/controller/ChatController.java
+++ backend/src/main/java/com/team5/secondhand/chat/bubble/controller/ChatController.java
@@ -2,25 +2,27 @@ package com.team5.secondhand.chat.bubble.controller;
 
 import com.team5.secondhand.chat.bubble.domain.ChatBubble;
 import com.team5.secondhand.chat.bubble.service.RedisMessagePublisher;
-import com.team5.secondhand.chat.bubble.service.RedisMessageSubscriber;
+import com.team5.secondhand.chat.noti.dto.ChatNotification;
+import com.team5.secondhand.chat.noti.service.SendChatNotificationUsecase;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequiredArgsConstructor
-@RestController
 @Slf4j
+@RestController
+@RequiredArgsConstructor
 class ChatController {
     private final RedisMessagePublisher redisMessagePublisher;
     private final ChannelTopic channelTopic;
+    private final SendChatNotificationUsecase chatNotification;
 
     @MessageMapping("/message")
     public void message(ChatBubble message) {
         log.debug("pub controller");
         redisMessagePublisher.publish(channelTopic.getTopic(), message);
+        chatNotification.sendChatNotificationToMember(message.getSender(), ChatNotification.of(message));
     }
 
 }
