e7c36346d9a6ba6603dea35d853b7f91c7d00c78
jaea-kim <jaeakim98@gmail.com>
2023-08-02 23:04:18 +0900

    [#325] FEAT: 에러 핸들러 등록

--- backend/src/main/java/com/team5/secondhand/chat/config/WebSocketMessageBroker.java
+++ backend/src/main/java/com/team5/secondhand/chat/config/WebSocketMessageBroker.java
@@ -1,14 +1,12 @@
-package com.team5.secondhand.chat.bubble.config;
+package com.team5.secondhand.chat.config;
 
+import com.team5.secondhand.chat.chatroom.handler.StompErrorHandler;
+import com.team5.secondhand.chat.chatroom.handler.StompMessageProcessor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
-import org.springframework.messaging.support.ChannelInterceptor;
-import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -19,12 +17,13 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketMessageBroker implements WebSocketMessageBrokerConfigurer {
     private final StompMessageProcessor stompMessageProcessor;
+    private final StompErrorHandler stompErrorHandler;
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/chat")
                 .setAllowedOrigins("*");
-//                .addInterceptors() //? 세션을 intercepter 에서 등록해주는 것인가?
+        registry.setErrorHandler(stompErrorHandler);
     }
 
     @Override
