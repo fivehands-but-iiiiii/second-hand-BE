609037011897ee8819bb72d7156691e54414beb7
saehim <saeilee.vxv@gmail.com>
2023-08-02 21:47:19 +0900

    [#329] FEAT: 채팅 메시지 발행 event 시 log 저장

--- backend/src/main/java/com/team5/secondhand/chat/bubble/service/RedisMessagePublisher.java
+++ backend/src/main/java/com/team5/secondhand/chat/bubble/service/RedisMessagePublisher.java
@@ -1,9 +1,11 @@
 package com.team5.secondhand.chat.bubble.service;
 
 import com.team5.secondhand.chat.bubble.domain.ChatBubble;
+import com.team5.secondhand.global.event.ChatBubbleArrivedEvent;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
@@ -12,13 +14,14 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Service
 public class RedisMessagePublisher {
+    private final ApplicationEventPublisher eventPublisher;
     private final RedisTemplate<String, Object> redisTemplate;
-    private final ChatLogService chatLogService;
 
     public void publish(String topic, ChatBubble message) {
         log.debug("pub log : " +  message.toString() + "/ topic: " + topic);
         message.ready();
-        chatLogService.saveChatBubble(message);
         redisTemplate.convertAndSend(topic, message);
+
+        eventPublisher.publishEvent(new ChatBubbleArrivedEvent(message));
     }
 }
