bafa1c23ec514939fa5f9645a71005788e178979
new-pow <103120173+new-pow@users.noreply.github.com>
2023-08-03 18:28:41 +0900

    [#329] FIX: Transactional import 누락 추가

--- backend/src/main/java/com/team5/secondhand/chat/notification/service/NotificationService.java
+++ backend/src/main/java/com/team5/secondhand/chat/notification/service/NotificationService.java
@@ -11,6 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import javax.servlet.http.HttpServletResponse;
