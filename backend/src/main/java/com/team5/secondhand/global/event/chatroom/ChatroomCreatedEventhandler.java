55a3768f5d011aee4e939360236f78701b71a078
saehim <saeilee.vxv@gmail.com>
2023-08-14 21:30:10 +0900

    [#344] FEAT: chatroom event 발생시 meta info 생성 및 저장 sub event 구현
    - #344

--- backend/src/main/java/com/team5/secondhand/global/event/chatroom/ChatroomCreatedEventhandler.java
+++ backend/src/main/java/com/team5/secondhand/global/event/chatroom/ChatroomCreatedEventhandler.java
@@ -1,8 +1,22 @@
 package com.team5.secondhand.global.event.chatroom;
 
+import com.team5.secondhand.api.chatroom.dto.ChatroomInfo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class ChatroomCreatedEventhandler {
 
+    private final ApplicationEventPublisher eventPublisher;
+
+    @Async
+    @EventListener
+    public void createChatroomInfo (ChatroomCreatedEvent event) {
+        ChatroomInfo info = event.getInfo();
+        eventPublisher.publishEvent(new ChatroomMetaInfoCreateEvent(info));
+    }
 }
