cde457a92fa0e4de72eee4ef24456f4133563211
jaea-kim <jaeakim98@gmail.com>
2023-08-21 19:25:56 +0900

    [#322] REMOVE: 필요없는 클래스 삭제

--- backend/src/main/java/com/team5/secondhand/chat/chatroom/repository/ChatroomMetaInfoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.team5.secondhand.chat.chatroom.repository;
-
-import com.team5.secondhand.chat.chatroom.domain.Chatroom;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ChatroomMetaInfoRepository extends MongoRepository<Chatroom, String> {
-}
