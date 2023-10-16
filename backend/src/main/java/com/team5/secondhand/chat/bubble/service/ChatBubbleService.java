abf600186139d3f7ec50bf9346f77512b75ab003
saehim <saeilee.vxv@gmail.com>
2023-10-16 20:35:18 +0900

    [#418] REFACTOR: `chatBubble` 상수 주입 방식 변경

--- backend/src/main/java/com/team5/secondhand/chat/bubble/service/ChatBubbleService.java
+++ backend/src/main/java/com/team5/secondhand/chat/bubble/service/ChatBubbleService.java
@@ -2,34 +2,38 @@ package com.team5.secondhand.chat.bubble.service;
 
 import com.team5.secondhand.chat.bubble.domain.ChatBubble;
 import com.team5.secondhand.chat.bubble.repository.ChatBubbleRepository;
+import com.team5.secondhand.chat.topic.service.RedisChatPublisher;
+import com.team5.secondhand.global.properties.ConstProperties;
 import com.team5.secondhand.global.event.chatbubble.ChatBubbleArrivedEvent;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.event.EventListener;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.scheduling.annotation.Async;
+import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@RequiredArgsConstructor
 public class ChatBubbleService {
-    @Value("${const.chat.bucket}")
-    private String chatBucketPrefix;
-    @Value("${const.chat.page-size}")
-    private int chatLoadSize;
+
+    private final ConstProperties.Chat chatContext;
+    private final RedisChatPublisher redisChatPublisher;
+    private final ApplicationEventPublisher publisher;
     private final ChatBubbleRepository repository;
-    private final RedisTemplate<String, ChatBubble> redisChatBubbleTemplate;
+
+    public ChatBubbleService(ConstProperties properties, ChatBubbleRepository repository, RedisChatPublisher redisChatPublisher, ApplicationEventPublisher publisher) {
+        this.chatContext = properties.getChat();
+        this.repository = repository;
+        this.redisChatPublisher = redisChatPublisher;
+        this.publisher = publisher;
+    }
 
     @Transactional(readOnly = true)
     public Slice<ChatBubble> getChatBubbles(int page, String roomId) {
         String key = generateChatLogKey(roomId);
-        Pageable pageable = PageRequest.of(page, chatLoadSize, Sort.by("createdAt").ascending());
-        Slice<ChatBubble> list = repository.findAllByRoomId(roomId, pageable);
+        Pageable pageable = PageRequest.of(page, chatContext.getPageSize(), Sort.by("createdAt").ascending());
+        Slice<ChatBubble> list = repository.findAllByRoomId(key, pageable);
         return list;
     }
 
@@ -38,14 +42,15 @@ public class ChatBubbleService {
         return repository.save(key, chatBubble);
     }
 
-    @Async
-    @EventListener
-    public void getChatBubble(ChatBubbleArrivedEvent chatBubbleArrivedEvent) {
-        ChatBubble chatBubble = chatBubbleArrivedEvent.getChatBubble();
-        saveChatBubble(chatBubble);
+    private String generateChatLogKey (String roomId) {
+        return String.format("%s%s:logs", chatContext.getBucket(), roomId);
     }
 
-    private String generateChatLogKey (String roomId) {
-        return String.format("%s%s:logs", chatBucketPrefix, roomId);
+    @Transactional
+    public void handleMessage(ChatBubble message) {
+        saveChatBubble(message);
+
+        redisChatPublisher.publish(message);
+        publisher.publishEvent(new ChatBubbleArrivedEvent(message));
     }
 }
