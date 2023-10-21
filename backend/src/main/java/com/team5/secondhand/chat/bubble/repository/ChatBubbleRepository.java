1dcfe65023260a82b79a721db52a289851f68599
saehim <saeilee.vxv@gmail.com>
2023-10-21 22:27:10 +0900

    [#418] FEAT: chatroom_bubble entity 구현

--- backend/src/main/java/com/team5/secondhand/chat/bubble/repository/ChatBubbleRepository.java
+++ backend/src/main/java/com/team5/secondhand/chat/bubble/repository/ChatBubbleRepository.java
@@ -1,6 +1,7 @@
 package com.team5.secondhand.chat.bubble.repository;
 
 import com.team5.secondhand.chat.bubble.domain.ChatBubble;
+import com.team5.secondhand.chat.bubble.repository.entity.ChatBubbleEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.*;
 import org.springframework.stereotype.Component;
@@ -13,13 +14,13 @@ public class ChatBubbleRepository {
 
     private final SpringDataChatBubbleRepository repository;
 
-    public Slice<ChatBubble> findAll(Pageable pageable) {
+    public Slice<ChatBubbleEntity> findAll(Pageable pageable) {
         pageable = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize() + 1, Sort.by("createdAt").ascending());
-        Page<ChatBubble> chatBubbles = repository.findAll(pageable);
+        Page<ChatBubbleEntity> chatBubbles = repository.findAll(pageable);
         return new SliceImpl(chatBubbles.getContent().subList(0, Math.min(pageable.getPageSize(), chatBubbles.getContent().size())), pageable, chatBubbles.getSize() > chatBubbles.getSize());
     }
 
-    public void saveAll(List<ChatBubble> allChatBubble) {
+    public void saveAll(List<ChatBubbleEntity> allChatBubble) {
         repository.saveAll(allChatBubble);
     }
 }
--- backend/src/main/java/com/team5/secondhand/chat/bubble/repository/SpringDataChatBubbleRepository.java
+++ backend/src/main/java/com/team5/secondhand/chat/bubble/repository/SpringDataChatBubbleRepository.java
@@ -1,9 +1,10 @@
 package com.team5.secondhand.chat.bubble.repository;
 
 import com.team5.secondhand.chat.bubble.domain.ChatBubble;
+import com.team5.secondhand.chat.bubble.repository.entity.ChatBubbleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SpringDataChatBubbleRepository extends JpaRepository<ChatBubble, Long> {
+public interface SpringDataChatBubbleRepository extends JpaRepository<ChatBubbleEntity, Long> {
 }
