d1aa973d45717c3559651f16cf84bd7267681b20
saehim <saeilee.vxv@gmail.com>
2023-10-21 22:26:45 +0900

    [#418] REFACTOR: chatConstProperties 단순한 구조로 수정

--- backend/src/main/java/com/team5/secondhand/chat/bubble/service/ChatBubbleService.java
+++ backend/src/main/java/com/team5/secondhand/chat/bubble/service/ChatBubbleService.java
@@ -3,37 +3,29 @@ package com.team5.secondhand.chat.bubble.service;
 import com.team5.secondhand.chat.bubble.domain.ChatBubble;
 import com.team5.secondhand.chat.bubble.repository.ChatBubbleCache;
 import com.team5.secondhand.chat.bubble.repository.ChatBubbleRepository;
+import com.team5.secondhand.chat.bubble.repository.entity.ChatBubbleEntity;
 import com.team5.secondhand.global.event.chatbubble.ChatBubbleArrivedEvent;
-import com.team5.secondhand.global.properties.ConstProperties;
+import com.team5.secondhand.global.properties.ChatConstProperties;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
-import org.springframework.data.domain.Sort;
-import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
+@RequiredArgsConstructor
 public class ChatBubbleService {
 
-    private final ConstProperties.Chat chatContext;
+    private final ChatConstProperties chatContext;
 
     private final RedisChatPublisher redisChatPublisher;
     private final ApplicationEventPublisher publisher;
 
     private final ChatBubbleCache bubbleCache;
     private final ChatBubbleRepository bubbleRepository;
-
-    protected ChatBubbleService(ConstProperties properties, ChatBubbleCache bubbleCache, RedisChatPublisher redisChatPublisher, ApplicationEventPublisher publisher, ChatBubbleRepository bubbleRepository) {
-        this.chatContext = properties.getChat();
-        this.bubbleCache = bubbleCache;
-        this.redisChatPublisher = redisChatPublisher;
-        this.publisher = publisher;
-        this.bubbleRepository = bubbleRepository;
-    }
 
     @Transactional(readOnly = true)
     public Slice<ChatBubble> getChatBubbles(int page, String roomId) {
@@ -44,7 +36,13 @@ public class ChatBubbleService {
         if (!list.hasContent()) {
             int lastCachePage = bubbleCache.getLastPage(key, chatContext.getPageSize());
             pageable = PageRequest.of(page - lastCachePage, chatContext.getPageSize(), Sort.by("createdAt").ascending());
-            return bubbleRepository.findAll(pageable);
+
+            Slice<ChatBubbleEntity> all = bubbleRepository.findAll(pageable);
+            List<ChatBubble> collect = all.getContent().stream()
+                    .map(ChatBubbleEntity::toDomain)
+                    .collect(Collectors.toList());
+
+            return new SliceImpl<>(collect.subList(0, Math.min(pageable.getPageSize(), all.getContent().size())), pageable, all.getSize() > pageable.getPageSize());
         }
 
         return list;
