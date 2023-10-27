3d8cd20b197ebe7aa5d26bdb9a498991a6eebbba
jaea-kim <jaeakim98@gmail.com>
2023-10-27 18:39:24 +0900

    [#435] REMOVE: 사용 안하는 인터셉터 삭제

--- backend/src/main/java/com/team5/secondhand/global/config/WebConfig.java
+++ backend/src/main/java/com/team5/secondhand/global/config/WebConfig.java
@@ -1,12 +1,10 @@
 package com.team5.secondhand.global.config;
 
-import com.team5.secondhand.global.auth.interceptor.AuthorityInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.server.CookieSameSiteSupplier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -15,13 +13,6 @@ public class WebConfig implements WebMvcConfigurer {
     private String testDomain;
     @Value("${server.address.was2}")
     private String prodDomain;
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new AuthorityInterceptor())
-                .addPathPatterns(AuthorityInterceptor.LOGIN_ESSENTIAL)
-                .excludePathPatterns(AuthorityInterceptor.LOGIN_INESSENTIAL);
-    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
