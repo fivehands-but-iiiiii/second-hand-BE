6dba7d526ecef9a96851563faf8cda11c424e9ca
saehim <saeilee.vxv@gmail.com>
2023-11-29 18:27:56 +0900

    [#466] FEAT: 기본 캐싱 키 상수화
    - 별도 인터페이스에 상수 선언

--- backend/src/main/java/com/team5/secondhand/global/config/CacheConfig.java
+++ backend/src/main/java/com/team5/secondhand/global/config/CacheConfig.java
@@ -1,20 +1,21 @@
 package com.team5.secondhand.global.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.time.Duration;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 
-import java.time.Duration;
-
-@Slf4j
 @Configuration
+@EnableCaching
 @RequiredArgsConstructor
 public class CacheConfig {
 
@@ -22,18 +23,29 @@ public class CacheConfig {
     private final ObjectMapper objectMapper;
 
     @Bean
-    public RedisCacheManager redisCacheManager() {
+    public CacheManager redisCacheManager() {
         return RedisCacheManager.RedisCacheManagerBuilder
                 .fromConnectionFactory(redisConnectionFactory)
-                .cacheDefaults(cacheConfiguration())
+                .cacheDefaults(defaultCacheConfiguration())
+                .withInitialCacheConfigurations(CacheConfigurations())
                 .build();
     }
 
-    @Bean
-    public RedisCacheConfiguration cacheConfiguration() {
-         return RedisCacheConfiguration.defaultCacheConfig()
-                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new JdkSerializationRedisSerializer()))
+    @Bean(name = "defaultCacheConfiguration")
+    public RedisCacheConfiguration defaultCacheConfiguration() {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(
+                        new GenericJackson2JsonRedisSerializer()))
                 .entryTtl(Duration.ofSeconds(3))
                 .disableCachingNullValues();
+    }
+
+    @Bean
+    public Map<String, RedisCacheConfiguration> CacheConfigurations() {
+        return Map.of(
+                CacheKey.MEMBER, defaultCacheConfiguration().entryTtl(Duration.ofSeconds(5)),
+                CacheKey.ITEM, defaultCacheConfiguration().entryTtl(Duration.ofSeconds(3)),
+                CacheKey.REGION, defaultCacheConfiguration().entryTtl(Duration.ofSeconds(100))
+        );
     }
 }
---
+++ backend/src/main/java/com/team5/secondhand/global/config/CacheKey.java
@@ -0,0 +1,8 @@
+package com.team5.secondhand.global.config;
+
+public interface CacheKey {
+    String MEMBER = "member";
+    String ITEM = "item";
+    String REGION = "region";
+    String CHAT = "chat";
+}
