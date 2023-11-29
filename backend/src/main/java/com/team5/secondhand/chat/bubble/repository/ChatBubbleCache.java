f089b57edf52eb4ac46523e21da1ffb40b0aac55
saehim <saeilee.vxv@gmail.com>
2023-11-29 18:26:06 +0900

    [#466] FEAT: bubble cache 구현

--- backend/src/main/java/com/team5/secondhand/chat/bubble/repository/ChatBubbleCache.java
+++ backend/src/main/java/com/team5/secondhand/chat/bubble/repository/ChatBubbleCache.java
@@ -1,49 +1,65 @@
 package com.team5.secondhand.chat.bubble.repository;
 
 import com.team5.secondhand.chat.bubble.domain.ChatBubble;
+import com.team5.secondhand.global.util.RedisOperationsHelper;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.SliceImpl;
-import org.springframework.data.redis.core.ListOperations;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
-public class ChatBubbleCache{
+public class ChatBubbleCache {
 
-//    private final RedisTemplate<String, ChatBubble> redisChatBubbleTemplate;
-//
-//    private long getStartIndex(Pageable page) {
-//        return (-1L * page.getPageSize() * page.getPageNumber()) - 1;
-//    }
-//
-//    private Slice<ChatBubble> getSlice(List<ChatBubble> messages, Pageable pageable) {
-//        boolean hasNext = false;
-//
-//        if (messages.size() > pageable.getPageSize()) {
-//            hasNext = true;
-//            messages.remove(pageable.getPageSize());
-//        }
-//
-//        return new SliceImpl<>(messages, pageable, hasNext);
-//    }
-//
-//    public Slice<ChatBubble> findAllByRoomId(String key, Pageable pageable) {
-//        ListOperations<String, ChatBubble> listOperations = redisChatBubbleTemplate.opsForList();
-//
-//        long startIndex = getStartIndex(pageable);
-//        long endIndex = startIndex - pageable.getPageSize();
-//
-//        List<ChatBubble> messages = listOperations.range(key, endIndex, startIndex);
-//        return getSlice(messages, pageable);
-//    }
-//
-//    public ChatBubble save(String key, ChatBubble chatBubble) {
-//        redisChatBubbleTemplate.opsForList().rightPush(key, chatBubble); // 캐시 저장
-//        return redisChatBubbleTemplate.opsForValue().get(key);
-//    }
+    private final RedisOperationsHelper operationsHelper;
+
+    private long getStartIndex(Pageable page) {
+        return (-1L * page.getPageSize() * page.getPageNumber());
+    }
+
+    private Slice<ChatBubble> getSlice(List<ChatBubble> messages, Pageable pageable) {
+        if (messages.isEmpty()) {
+            throw new IndexOutOfBoundsException("빈 페이지 입니다.");
+        }
+
+        if (messages.size() > pageable.getPageSize()) {
+            messages.remove(pageable.getPageSize());
+        }
+
+        return new SliceImpl<>(
+                messages.subList(0, Math.min(pageable.getPageSize(), messages.size())), pageable,
+                pageable.getPageSize() < messages.size());
+    }
+
+    public Slice<ChatBubble> findAllByRoomId(String key, Pageable pageable) {
+
+        long startIndex = getStartIndex(pageable);
+        long endIndex = startIndex - pageable.getPageSize();
+
+        List<ChatBubble> messages = operationsHelper.getList(key, endIndex - 1, startIndex,
+                ChatBubble.class);
+
+        return getSlice(messages, pageable);
+    }
+
+    public List<ChatBubble> findAllByRoomId(String key) {
+        long size = operationsHelper.size(key);
+        return operationsHelper.getList(key, 0, size, ChatBubble.class);
+    }
+
+    public ChatBubble save(String key, ChatBubble chatBubble) {
+        operationsHelper.putToList(key, chatBubble);
+        return chatBubble;
+    }
+
+    public int getLastPage(String key, int pageSize) {
+        Long size = operationsHelper.size(key);
+        return (int) (size / pageSize);
+    }
+
+    public void clear(String key) {
+        operationsHelper.delete(key);
+    }
 }
